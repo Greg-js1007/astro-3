5a36750b1212b563d016690dd4f581137d2b76c2
Gregory Subero <suberog010@gmail.com>
2025-04-11 03:54:55 +0000

    cambios en las configuraciones de astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    css: {
+      postcss: {
+        plugins: [tailwind(), autoprefixer()],
+      },
+    },
+  },
+});
